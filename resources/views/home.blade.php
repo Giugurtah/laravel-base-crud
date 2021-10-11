22093a225b1770e814254fd5a8e38715d4974b73
Giugurtah <c.buoncompagni.98@gmail.com>
2021-10-11 15:44:43 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('title', 'Home')
+    
+@section('content')
+    <main>
+        <div class="card w-50 mt-5 mx-auto">
+            <div class="card-body text-center">
+              <img src="https://www.pngarts.com/files/1/DC-Comics-PNG-Background-Image.png" width="200" alt="">
+              <h2 class="mt-3 brand_color">DC COMICS</h2>
+            </div>
+          </div>
+    </main>
+@endsection
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">
+          <img src="{{ asset('./img/logo.png') }}" alt="dc logo" width="50" class="d-inline-block img-fluid" alt="">
+      </a>
+      <div class="flex-grow-1">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link {{ request()->routeIs('home') ? 'active' : '' }}" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Comics</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
